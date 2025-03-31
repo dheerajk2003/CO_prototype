c5e26c3bd82177b90cdc0784b19a31bfaf166358
dheerajk2003 <dheerajkhatri2003@gmail.com>
2025-03-31 21:24:39 +0530

    modified syllabus extractor to extract custom strings

--- SyllabusExtractor.py
+++ SyllabusExtractor.py
@@ -1,6 +1,6 @@
 import PyPDF2
 
-def extract_text_from_pdf(pdf_path):
+def extract_text_from_pdf(pdf_path, start, end, s_end=""):
     wt = open("data.txt", 'w')
     with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
@@ -11,11 +11,11 @@ def extract_text_from_pdf(pdf_path):
     end_idx = 0
     start_idx = 0
     try:
-        start_idx = text.lower().index("syllabus")
-        end_idx = text.lower().index("textbook")
+        start_idx = text.lower().index(start)
+        end_idx = text.lower().index(end)
     except ValueError:
         try:
-            end_idx = text.lower().index("text book")
+            end_idx = text.lower().index(s_end)
         except:
             end_idx = len(text)
 
@@ -25,5 +25,7 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 pdf_path = '/home/dheeraj/Downloads/CAP6206 HANDOUTS-Student.pdf'
-pdf_text = extract_text_from_pdf(pdf_path)
-# print(pdf_text)  # Print the first 500 characters
+pdf_text = extract_text_from_pdf(pdf_path, "syllabus", "textbook", "text book")
+print(pdf_text)
+pdf_text = extract_text_from_pdf(pdf_path, "course outcome", "program specific outcome")
+print(pdf_text)
