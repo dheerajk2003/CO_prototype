5947dada11bd2909c4e62344e8e83bef11e3a460
dheerajk2003 <dheerajkhatri2003@gmail.com>
2025-04-02 23:37:25 +0530

    integrated extract data into main and also created get worst score section method

--- MTE_data_extractor.py
+++ MTE_data_extractor.py
@@ -2,19 +2,55 @@ from openpyxl import load_workbook
 from openpyxl.utils import column_index_from_string
 
 # data_only=true returns calculated value if formula is applied on that particular cell
-workbook = load_workbook("sample.xlsm", data_only=True)
-worksheet = workbook["MTE"]
-students = [[]]
-rollNo_col = column_index_from_string("B")
-N_value_col = [4, 6, 8, 10, 12, 14, 16, 18]
 
-for row_index in range(6, 176):
-    i = 0
-    # extracts individual cell values
-    rollNo = worksheet.cell(row=row_index, column=rollNo_col).value
-    # for loop helps getting all the values mentioned in the list above
-    N_value = [worksheet.cell(row=row_index, column=col).value for col in N_value_col]
+def extract_data(file_path, worksheet_name, rowList, colList, ):
+    workbook = load_workbook(file_path, data_only=True)
+    worksheet = workbook[worksheet_name]
+    students = []
+    rollNo_col = column_index_from_string("B")
+    N_value_col = colList
 
-    students[i].append(N_value)  # to insert data in a 2d list
-    print(students)
-    i += 1
+    for row_index in range(rowList[0], rowList[1]):
+        # extracts individual cell values
+        rollNo = worksheet.cell(row=row_index, column=rollNo_col).value
+        # for loop helps getting all the values mentioned in the list above
+        N_value = [worksheet.cell(row=row_index, column=col).value for col in N_value_col]
+
+        students.append(N_value)  # to insert data in a 2d list
+        # print(students)
+
+    return students
+
+def get_worst(students):
+    performance = []
+    for i in range(len(students)):
+        performance.append([0,0])
+        a = round(sum(students[i][0:3]) / 30 * 100, 2)
+        b = round(sum(students[i][3:7]) / 40 * 100, 2)
+        c = (students[i][7]) * 10
+
+        if(a < b and a < c):
+            performance[i][0] = 0
+            performance[i][1] = a
+        elif(b < a and b < c):
+            performance[i][0] = 1
+            performance[i][1] = b
+        else:
+            performance[i][0] = 2
+            performance[i][1] = c
+
+
+    return performance
+
+
+# students = extract_data("test.xlsm", "MTE", [6, 20], [4, 6, 8, 10, 12, 14, 16, 18])
+
+# print(get_worst(students))
+# print(students[0], students[-1])
+
+
+
+
+#[4, 6, 8, 10, 12, 14, 16, 18]
+
+# 6, 176
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from GetTest import GetTest
 from SyllabusExtractor import extract_text_from_pdf
+from MTE_data_extractor import extract_data
+from MTE_data_extractor import get_worst
 
-pdf_path = "/home/dheeraj/Downloads/CAP6206 HANDOUTS-Student.pdf"
+
+students = extract_data("test.xlsm", "MTE", [6, 176], [4, 6, 8, 10, 12, 14, 16, 18])
+worst = get_worst(students)
+pdf_path = "/home/dheeraj/Downloads/CAP6205 Course Handout OS.pdf"
 syllabus = extract_text_from_pdf(pdf_path, "syllabus", "textbook", "text book")
 type_details = extract_text_from_pdf(pdf_path, "course outcome", "program specific outcome")
 type_info = ["understanding", "apply", "analyze"]
-type_num = 2
+# type_num = 2
+
+roll_no = int(input("Enter the roll no of student that you want to get test for: ")) - 1
 
 content = {
     "contents": {
         "parts" : [
             {"text" : "Generate a test According to given syllabus and type/section of test (section a = understanding, section b = apply, section c = analyze)"},
             {"text": syllabus + type_details },
-            {"text": "the type/section of text is " + type_info[type_num] + "give test of only that section" }
+            {"text": "the type/section of text is " + type_info[worst[roll_no][0]] + "give test of only that section," },
+            {"text": "the student secured " + str(worst[roll_no][1]) + " percentage in that particular section so generate test accodingly"}
         ]
     }
 }
