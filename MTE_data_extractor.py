86d1f6dcac33205e0bbf1faa569308a36112079a
dheerajk2003 <dheerajkhatri2003@gmail.com>
2025-04-10 16:32:20 +0530

    conflict 2

--- MTE_data_extractor.py
+++ MTE_data_extractor.py
@@ -1,9 +1,15 @@
 from openpyxl import load_workbook
 from openpyxl.utils import column_index_from_string
-
+from collections import defaultdict
 # data_only=true returns calculated value if formula is applied on that particular cell
 
-def extract_data(file_path, worksheet_name, rowList, colList, ):
+
+def extract_data(
+    file_path,
+    worksheet_name,
+    rowList,
+    colList,
+):
     workbook = load_workbook(file_path, data_only=True)
     worksheet = workbook[worksheet_name]
     students = []
@@ -14,7 +20,9 @@ def extract_data(file_path, worksheet_name, rowList, colList, ):
         # extracts individual cell values
         rollNo = worksheet.cell(row=row_index, column=rollNo_col).value
         # for loop helps getting all the values mentioned in the list above
-        N_value = [worksheet.cell(row=row_index, column=col).value for col in N_value_col]
+        N_value = [
+            worksheet.cell(row=row_index, column=col).value for col in N_value_col
+        ]
 
         students.append(N_value)  # to insert data in a 2d list
         # print(students)
@@ -31,3 +39,44 @@ def get_worst(students):
         performance[i][2] = (students[i][7]) * 10
 
     return performance
+
+
+def get_CO_Cluster(performance):
+    student_wise_CO_cluster = {}
+    for i, j in enumerate(performance):
+        temp2 = []
+        for a, b in enumerate(j):
+            if b < 65:
+                temp2.append(1)
+            else:
+                temp2.append(0)
+        str1 = int(("".join(str(x) for x in temp2)), 2)
+
+        student_wise_CO_cluster[i] = str1
+
+    cluster = defaultdict(list)
+
+    for i in student_wise_CO_cluster:
+        match student_wise_CO_cluster[i]:
+            case 0:
+                cluster[0].append(i)
+            case 1:
+                cluster[1].append(i)
+            case 2:
+                cluster[2].append(i)
+            case 3:
+                cluster[3].append(i)
+            case 4:
+                cluster[4].append(i)
+            case 5:
+                cluster[5].append(i)
+            case 6:
+                cluster[6].append(i)
+            case 7:
+                cluster[7].append(i)
+
+    CO_cluster = {k: cluster[k] for k in sorted(cluster)}
+
+
+# for i in CO_cluster:
+#     print(f"{i} = {CO_cluster[i]}")
