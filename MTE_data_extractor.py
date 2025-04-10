a96593e15ebac44d4ec7be6f29021fe70feb02e2
dheerajk2003 <dheerajkhatri2003@gmail.com>
2025-04-10 16:20:47 +0530

    conflict resolving

--- MTE_data_extractor.py
+++ MTE_data_extractor.py
@@ -21,26 +21,13 @@ def extract_data(file_path, worksheet_name, rowList, colList, ):
 
     return students
 
-def get_worst(students, mapping):
-    
-    length = max(mapping)
-    count = [0]*length
-    for i in range(len(mapping)):
-        count[mapping[i]-1] += 1
+
+def get_worst(students):
     performance = []
     for i in range(len(students)):
-
-        performance_temp = [0]*length
-        for j in range(len(mapping)):
-            performance_temp[mapping[j] - 1] += students[i][j]
-        for j in range(length):
-            performance_temp[j] = round(performance_temp[j] / count[j] * 10, 2)
-
-        performance.append(performance_temp)
-        # performance[i][0] = round(sum(students[i][0:3]) / 30 * 100, 2)
-        # performance[i][1] = round(sum(students[i][3:7]) / 40 * 100, 2)
-        # performance[i][2] = (students[i][7]) * 10
-
+        performance.append([0, 0, 0])
+        performance[i][0] = round(sum(students[i][0:3]) / 30 * 100, 2)
+        performance[i][1] = round(sum(students[i][3:7]) / 40 * 100, 2)
+        performance[i][2] = (students[i][7]) * 10
 
     return performance
-
--- main.py
+++ main.py
@@ -2,45 +2,57 @@ from ai.handleAI import handleAI
 from SyllabusExtractor import extract_text_from_pdf
 from MTE_data_extractor import extract_data
 from MTE_data_extractor import get_worst
-
-#modificatoin
-from ai.GetTest import Gemini
-from Clustering import Clustering
-c = Clustering()
-gemini = Gemini()
-
-# aiHandler = handleAI()
+from MTE_data_extractor import get_CO_Cluster
 
 
-# data form excel
-students = extract_data("data/test.xlsm", "MTE", [6, 176], [4, 6, 8, 10, 12, 14, 16, 18])
-#data form exam paper for co mapping
-paper = extract_text_from_pdf("data/CA2106_OS__MTE-24.pdf")
+aiHandler = handleAI()
 
-co_map = gemini.getMapping(paper)
-print(co_map)
-performance = get_worst(students, co_map)
-
-# data from handout
+students = extract_data(
+    "data/test.xlsm", "MTE", [6, 176], [4, 6, 8, 10, 12, 14, 16, 18]
+)
+performance = get_worst(students)
+CO_Cluster = get_CO_Cluster(performance)
 pdf_path = "data/CAP6205 Course Handout OS.pdf"
 syllabus = extract_text_from_pdf(pdf_path, "syllabus", "textbook", "text book")
-type_details = extract_text_from_pdf(pdf_path, "course outcome", "program specific outcome")
+type_details = extract_text_from_pdf(
+    pdf_path, "course outcome", "program specific outcome"
+)
 type_info = ["understanding", "apply", "analyze"]
 
-# clustering
-print(c.clustering(performance))
 
 roll_no = int(input("Enter the roll no of student that you want to get test for: ")) - 1
 
-content = "Generate a test According to given syllabus and type/section of test " + syllabus + type_details
-content += "(percentage of co1 is "+str(performance[roll_no][0])+"percentage of co2 is"+str(performance[roll_no][1])+"percentage of co3 is"+str(performance[roll_no][2])+")"
+content = (
+    "Generate a test According to given syllabus and type/section of test "
+    + syllabus
+    + type_details
+)
+content += (
+    "(percentage of co1 is "
+    + str(performance[roll_no][0])
+    + "percentage of co2 is"
+    + str(performance[roll_no][1])
+    + "percentage of co3 is"
+    + str(performance[roll_no][2])
+    + ")"
+)
 content += "add questions of only that co in which the student has secured less than 65 percentage"
 content += "there should be total 8 question divided between diffent sections according to co percentage, there should be more questions of co in which student has performed worst."
 
-if(performance[roll_no][0] > 65 and performance[roll_no][1] > 65 and performance[roll_no][2] > 65):
+if (
+    performance[roll_no][0] > 65
+    and performance[roll_no][1] > 65
+    and performance[roll_no][2] > 65
+):
     print("no need to generate test for this student, this student has good grades")
-    
+
 else:
-    print("co1 = "+str(performance[roll_no][0]) + " co2 = " + str(performance[roll_no][1]) + " co3 = " + str(performance[roll_no][2]))
-    # aiHandler.GenerateResponses(content)
-    print(gemini.GetTest(content))
+    print(
+        "co1 = "
+        + str(performance[roll_no][0])
+        + " co2 = "
+        + str(performance[roll_no][1])
+        + " co3 = "
+        + str(performance[roll_no][2])
+    )
+    aiHandler.GenerateResponses(content)
